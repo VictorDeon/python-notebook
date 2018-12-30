a2f1a741f7bb597bdb7a2caeead95fcd0ed5306e
Victor Arnaud <victorhad@gmail.com>
2018-12-30 20:20:32 -0200

    Introdução a mineração de emoções

---
+++ 08_Inteligencia_Artificial/01_Mineracao_de_Emocoes_em_Textos/base.py
@@ -0,0 +1,28 @@
+base = [
+    ('eu sou admirada por muitos','alegria'),
+    ('me sinto completamente amado','alegria'),
+    ('amar e maravilhoso','alegria'),
+    ('estou me sentindo muito animado novamente','alegria'),
+    ('eu estou muito bem hoje','alegria'),
+    ('que belo dia para dirigir um carro novo','alegria'),
+    ('o dia está muito bonito','alegria'),
+    ('estou contente com o resultado do teste que fiz no dia de ontem','alegria'),
+    ('o amor e lindo','alegria'),
+    ('nossa amizade e amor vai durar para sempre', 'alegria'),
+    ('estou amedrontado', 'medo'),
+    ('ele esta me ameacando a dias', 'medo'),
+    ('isso me deixa apavorada', 'medo'),
+    ('este lugar e apavorante', 'medo'),
+    ('se perdermos outro jogo seremos eliminados e isso me deixa com pavor', 'medo'),
+    ('tome cuidado com o lobisomem', 'medo'),
+    ('se eles descobrirem estamos encrencados', 'medo'),
+    ('estou tremendo de medo', 'medo'),
+    ('eu tenho muito medo dele', 'medo'),
+    ('estou com medo do resultado dos meus testes', 'medo')
+]
+
+stopwords = [
+    'a', 'agora', 'algum', 'alguma', 'aquele', 'aqueles', 'de', 'deu', 'do', 'e', 'estou', 'esta', 'esta',
+    'ir', 'meu', 'muito', 'mesmo', 'no', 'nossa', 'o', 'outro', 'para', 'que', 'sem', 'talvez', 'tem', 'tendo',
+    'tenha', 'teve', 'tive', 'todo', 'um', 'uma', 'umas', 'uns', 'vou'
+]
