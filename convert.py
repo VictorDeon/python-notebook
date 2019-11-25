4b9301f0f934ab93f7d8a4f8b7ce622510a5b0ed
Victor Deon <victorhad@gmail.com>
2019-11-25 02:23:41 -0300

    fix convert.py

--- convert.py
+++ convert.py
@@ -13,3 +13,15 @@ for (name, subdirectories, files) in os.walk("."):
                 output_path = name.replace("./", "htmls/")
                 os.system(f"jupyter nbconvert --to html {path} --output-dir={output_path}")
                 print(f"{file_name} --> {output_path}")
+
+
+for (name, subdirectories, files) in os.walk("."):
+    if name.startswith("./htmls"):
+        continue
+
+    for file_name in files:
+        if os.path.splitext(file_name) == ("index", ".html"):
+            path = os.path.join(name, file_name)
+            output_path = path.replace("./", "htmls/")
+            os.system(f"cp {path} {output_path}")
+            print(f"cp: {path} --> {output_path}")
